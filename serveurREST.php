cddf03e75bb846128f249c5e955a23c48436a3e8
FlavienDIAS <115453321+FlavienDIAS@users.noreply.github.com>
2023-03-17 09:51:24 +0100

    Ajout de la verification des token

--- serveurREST.php
+++ serveurREST.php
@@ -24,8 +24,9 @@
     switch ($http_method){
         /// Cas de la méthode GET
         case "GET" :
-            /// Récupération des critères de recherche envoyés par le Client
+            ///verification de la validité du token
             if(is_jwt_valid($bearer_token)){
+               /// Récupération des critères de recherche envoyés par le Client 
                if (isset($_GET['id'])){
                     $req = $linkpdo->prepare("SELECT * FROM `article` WHERE id_article = ?;");
                     $req->execute(array($_GET['id']));
@@ -43,92 +44,115 @@
             }
         /// Cas de la méthode POST
         case "POST" :
-            /// Récupération des données envoyées par le Client
-            $postedData = file_get_contents('php://input');
-            $data = json_decode($postedData);
-            $contenue = $data->contenue;
-            $auteur = $data->auteur;
-            if (!empty($data->phrase)){
-                $req = $linkpdo->prepare("INSERT INTO article (contenue, id_utilisateur) VALUES (:contenue, :auteur)");
-                $req->execute(array('contenue' => $contenue, 'auteur'=>$auteur));
+            ///verification de la validité du token
+            if(is_jwt_valid($bearer_token)){
+                /// Récupération des données envoyées par le Client
+                $postedData = file_get_contents('php://input');
+                $data = json_decode($postedData);
+                $contenue = $data->contenue;
+                $auteur = $data->auteur;
+                if (!empty($data->phrase)){
+                    $req = $linkpdo->prepare("INSERT INTO article (contenue, id_utilisateur) VALUES (:contenue, :auteur)");
+                    $req->execute(array('contenue' => $contenue, 'auteur'=>$auteur));
+                }
+                /// Traitement
+                /// Envoi de la réponse au Client
+                deliver_response(200, "Votre message", NULL);
+                break;
+            }else{
+                echo "token invalide";
             }
-            /// Traitement
-            /// Envoi de la réponse au Client
-            deliver_response(200, "Votre message", NULL);
-            break;
         /// Cas de la méthode PUT
         case "PUT" :  
-            /// Traitement
+            ///verification de la validité du token
+            if(is_jwt_valid($bearer_token)){
             /// Envoi de la réponse au Client
-            $postedData = file_get_contents('php://input');
-            $postedData = json_decode($postedData);
-            if (!empty($postedData->phrase)){
-                $req = $linkpdo->prepare("UPDATE `article` SET `contenue` = :phrase WHERE `article`.`id_article` = :id;");
-                $req->execute(array('contenue' => $postedData->phrase,
-                                    'id' => $postedData->id));
+                $postedData = file_get_contents('php://input');
+                $postedData = json_decode($postedData);
+                if (!empty($postedData->phrase)){
+                    $req = $linkpdo->prepare("UPDATE `article` SET `contenue` = :phrase WHERE `article`.`id_article` = :id;");
+                    $req->execute(array('contenue' => $postedData->phrase,
+                                        'id' => $postedData->id));
+                }
+                deliver_response(201, "Votre message", NULL);
+                break;
+            }else{
+                echo "token invalide";
             }
-            deliver_response(201, "Votre message", NULL);
-            break;
-            /// Récupération des données envoyées par le Client
             
         /// Cas de la méthode DELETE
         case "DELETE" :
-            /// Récupération des données envoyées par le Client
-            if (isset($_GET['id'])){
-                $req = $linkpdo->prepare("DELETE FROM article WHERE `article`.`id_article` = ?");
-                $req->execute(array($_GET['id']));
+            ///verification de la validité du token
+            if(is_jwt_valid($bearer_token)){
+                /// Récupération des données envoyées par le Client
+                if (isset($_GET['id'])){
+                    $req = $linkpdo->prepare("DELETE FROM article WHERE `article`.`id_article` = ?");
+                    $req->execute(array($_GET['id']));
+                }
+                /// Traitement
+                /// Envoi de la réponse au Client
+                deliver_response(200, "Votre message", NULL);
+                break;
+            }else{
+                echo "token invalide";
             }
-            /// Traitement
-            /// Envoi de la réponse au Client
-            deliver_response(200, "Votre message", NULL);
-            break;
         /// Cas de la méthode LIKE
         case "LIKE" :
-            /// Récupération des données envoyées par le Client
-            $postedData = file_get_contents('php://input');
-            $data = json_decode($postedData);
-            $idUtilisateur = $data->idUtilisateur;
-            $idArticle = $data->idArticle;
-            if (!empty($data->idUtilisateur)){
-                if(!empty($data->idArticle)){
-                    ///verification de vote deja existant
-                    $req = $linkpdo->prepare("SELECT * FROM `reagir` WHERE id_article = :id_article AND id_utilisateur = :id_utilisateur;");
-                    $req->execute(array('id_article'=> $idArticle, 'id_utilisateur'=> $idUtilisateur));
-                    if($req = NULL){
-                        $req = $linkpdo->prepare("INSERT INTO reagir(id_utilisateur, id_article, liker, disliker) VALUES (:id_utilisateur, :id_article, :liker, :disliker)");
-                        $req->execute(array('id_utilisateur' => $idUtilisateur, 'id_article' => $idArticle, 'liker'=>1, 'disliker'=>0));
-                    }else{
-                        $req = $linkpdo->prepare("UPDATE `reagir` SET `liker` = :likefalse , 'disliker'= :disliketrue WHERE `reagir`.`id_article` = :id_article AND 'reagir'.'id_utilisateur'= :id_utilisateur;");
-                        $req->execute(array('id_article'=> $idArticle, 'id_utilisateur'=>$idUtilisateur,'likefalse'=>1, 'disliketrue'=>0));
-                    }
-                }   
-            }
-            /// Envoi de la réponse au Client
-            deliver_response(200, "Votre message", NULL);
-            break;
-        case "DISLIKE" :
-            /// Récupération des données envoyées par le Client
-            $postedData = file_get_contents('php://input');
-            $data = json_decode($postedData);
-            $idUtilisateur = $data->idUtilisateur;
-            $idArticle = $data->idArticle;
-            if(!empty($data->idUtilisateur)){
-                if(!empty($data->idArticle)){
-                    ///verification de vote deja existant
-                    $req = $linkpdo->prepare("SELECT * FROM `reagir` WHERE id_article = :id_article AND id_utilisateur = :id_utilisateur;");
-                    $req->execute(array('id_article'=> $idArticle, 'id_utilisateur'=> $idUtilisateur));
-                    if($req = NULL){
-                        $req = $linkpdo->prepare("INSERT INTO reagir(id_utilisateur, id_article, liker, disliker) VALUES (:id_utilisateur, :id_article, :liker, :disliker)");
-                        $req->execute(array('id_utilisateur' => $idUtilisateur, 'id_article' => $idArticle, 'liker'=>0, 'disliker'=>1));
-                    }else{
-                        $req = $linkpdo->prepare("UPDATE `reagir` SET `liker` = :likefalse , 'disliker'= :disliketrue WHERE `reagir`.`id_article` = :id_article AND 'reagir'.'id_utilisateur'= :id_utilisateur;");
-                        $req->execute(array('id_article'=> $idArticle, 'id_utilisateur'=>$idUtilisateur,'likefalse'=>0, 'disliketrue'=>1));
+            ///verification de la validité du token
+            if(is_jwt_valid($bearer_token)){
+                /// Récupération des données envoyées par le Client
+                $postedData = file_get_contents('php://input');
+                $data = json_decode($postedData);
+                $idUtilisateur = $data->idUtilisateur;
+                $idArticle = $data->idArticle;
+                if (!empty($data->idUtilisateur)){
+                    if(!empty($data->idArticle)){
+                        ///verification de vote deja existant
+                        $req = $linkpdo->prepare("SELECT * FROM `reagir` WHERE id_article = :id_article AND id_utilisateur = :id_utilisateur;");
+                        $req->execute(array('id_article'=> $idArticle, 'id_utilisateur'=> $idUtilisateur));
+                        if($req = NULL){
+                            $req = $linkpdo->prepare("INSERT INTO reagir(id_utilisateur, id_article, liker, disliker) VALUES (:id_utilisateur, :id_article, :liker, :disliker)");
+                            $req->execute(array('id_utilisateur' => $idUtilisateur, 'id_article' => $idArticle, 'liker'=>1, 'disliker'=>0));
+                        }else{
+                            $req = $linkpdo->prepare("UPDATE `reagir` SET `liker` = :likefalse , 'disliker'= :disliketrue WHERE `reagir`.`id_article` = :id_article AND 'reagir'.'id_utilisateur'= :id_utilisateur;");
+                            $req->execute(array('id_article'=> $idArticle, 'id_utilisateur'=>$idUtilisateur,'likefalse'=>1, 'disliketrue'=>0));
+                        }
                     }   
                 }
+                /// Envoi de la réponse au Client
+                deliver_response(200, "Votre message", NULL);
+                break;
+            }else{
+                echo "token invalide";
             }
-            ///Envoi de la réponse au Client
-            deliver_response(200,"message", NULL);
-            break;
+        case "DISLIKE" :
+            ///verification de la validité du token
+            if(is_jwt_valid($bearer_token)){
+                /// Récupération des données envoyées par le Client
+                $postedData = file_get_contents('php://input');
+                $data = json_decode($postedData);
+                $idUtilisateur = $data->idUtilisateur;
+                $idArticle = $data->idArticle;
+                if(!empty($data->idUtilisateur)){
+                    if(!empty($data->idArticle)){
+                        ///verification de vote deja existant
+                        $req = $linkpdo->prepare("SELECT * FROM `reagir` WHERE id_article = :id_article AND id_utilisateur = :id_utilisateur;");
+                        $req->execute(array('id_article'=> $idArticle, 'id_utilisateur'=> $idUtilisateur));
+                        if($req = NULL){
+                            $req = $linkpdo->prepare("INSERT INTO reagir(id_utilisateur, id_article, liker, disliker) VALUES (:id_utilisateur, :id_article, :liker, :disliker)");
+                            $req->execute(array('id_utilisateur' => $idUtilisateur, 'id_article' => $idArticle, 'liker'=>0, 'disliker'=>1));
+                        }else{
+                            $req = $linkpdo->prepare("UPDATE `reagir` SET `liker` = :likefalse , 'disliker'= :disliketrue WHERE `reagir`.`id_article` = :id_article AND 'reagir'.'id_utilisateur'= :id_utilisateur;");
+                            $req->execute(array('id_article'=> $idArticle, 'id_utilisateur'=>$idUtilisateur,'likefalse'=>0, 'disliketrue'=>1));
+                        }   
+                    }
+                }
+                ///Envoi de la réponse au Client
+                deliver_response(200,"message", NULL);
+                break;
+            }else{
+                echo "token invalide";
+            }
         //cas d'erreur
         default :
             /// Récupération de l'identifiant de la ressource envoyé par le Client
